c46535c3a3f644d539797ba1f0f03da494b58971
parshin <ilyaiparshin@gmail.com>
2020-04-06 23:15:46 +0500

    chapter 3 ended

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -86,3 +87,26 @@ def post_share(request, post_id):
                                                     'form': form,
                                                     'sent': sent})
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+
+    if form.is_valid():
+        query = form.cleaned_data['query']
+
+    # results = Post.objects.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+    search_vector = SearchVector('title', 'body')
+    search_query = SearchQuery(query)
+    results = Post.objects.annotate(
+        search=search_vector,
+        rank=SearchRank(search_vector, search_query)
+    ).filter(search=search_query).order_by('-rank')
+
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
